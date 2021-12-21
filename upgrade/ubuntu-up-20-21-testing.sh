212a13ff0c916260736a58d4399b135dc1841b53
/:) <g.yevgeniy.p@gmail.com>
2021-12-21 15:01:59 +0300

    + upgrade 20 to 21 (testing)

--- upgrade/ubuntu-up-20-21-testing.sh
+++ upgrade/ubuntu-up-20-21-testing.sh
@@ -253,15 +253,15 @@ sudo chmod 6750 /usr/local/sbin/gvmd
 # Sync
 # -------------------------------------------------------------------------------------------\
 
-# sudo chown gvm:gvm /usr/local/bin/greenbone-nvt-sync && \
-# sudo chmod 740 /usr/local/sbin/greenbone-feed-sync && \
-# sudo chown gvm:gvm /usr/local/sbin/greenbone-*-sync && \
-# sudo chmod 740 /usr/local/sbin/greenbone-*-sync
+sudo chown gvm:gvm /usr/local/bin/greenbone-nvt-sync && \
+sudo chmod 740 /usr/local/sbin/greenbone-feed-sync && \
+sudo chown gvm:gvm /usr/local/sbin/greenbone-*-sync && \
+sudo chmod 740 /usr/local/sbin/greenbone-*-sync
 
 # Allow GVM user use OpenVAS
 # -------------------------------------------------------------------------------------------\
 
-# echo "%gvm ALL = NOPASSWD: /usr/local/sbin/openvas" >> /etc/sudoers
+echo "%gvm ALL = NOPASSWD: /usr/local/sbin/openvas" >> /etc/sudoers
 
 # PostgreSQL
 # -------------------------------------------------------------------------------------------\
@@ -327,91 +327,90 @@ EOF
 # # Gen certs
 # # -------------------------------------------------------------------------------------------\
 
-# sudo -u gvm gvm-manage-certs -a
+sudo -u gvm gvm-manage-certs -a
 
 # # Gen systemd units
 # # -------------------------------------------------------------------------------------------\
 
-# cat << EOF > $BUILD_DIR/gvmd.service
-# [Unit]
-# Description=Greenbone Vulnerability Manager daemon (gvmd)
-# After=network.target networking.service postgresql.service ospd-openvas.service
-# Wants=postgresql.service ospd-openvas.service
-# Documentation=man:gvmd(8)
-# ConditionKernelCommandLine=!recovery
+cat << EOF > $BUILD_DIR/gvmd.service
+[Unit]
+Description=Greenbone Vulnerability Manager daemon (gvmd)
+After=network.target networking.service postgresql.service ospd-openvas.service
+Wants=postgresql.service ospd-openvas.service
+Documentation=man:gvmd(8)
+ConditionKernelCommandLine=!recovery
 
-# [Service]
-# Type=forking
-# User=gvm
-# Group=gvm
-# PIDFile=/run/gvm/gvmd.pid
-# RuntimeDirectory=gvm
-# RuntimeDirectoryMode=2775
-# ExecStart=/usr/local/sbin/gvmd --osp-vt-update=/run/ospd/ospd-openvas.sock --listen-group=gvm
-# Restart=always
-# TimeoutStopSec=10
+[Service]
+Type=forking
+User=gvm
+Group=gvm
+PIDFile=/run/gvm/gvmd.pid
+RuntimeDirectory=gvm
+RuntimeDirectoryMode=2775
+ExecStart=/usr/local/sbin/gvmd --osp-vt-update=/run/ospd/ospd-openvas.sock --listen-group=gvm
+Restart=always
+TimeoutStopSec=10
 
-# [Install]
-# WantedBy=multi-user.target
-# EOF
+[Install]
+WantedBy=multi-user.target
+EOF
 
-# sudo cp $BUILD_DIR/gvmd.service /etc/systemd/system/
+sudo cp $BUILD_DIR/gvmd.service /etc/systemd/system/
 
-# cat << EOF > $BUILD_DIR/gsad.service
-# [Unit]
-# Description=Greenbone Security Assistant daemon (gsad)
-# Documentation=man:gsad(8) https://www.greenbone.net
-# After=network.target gvmd.service
-# Wants=gvmd.service
+cat << EOF > $BUILD_DIR/gsad.service
+[Unit]
+Description=Greenbone Security Assistant daemon (gsad)
+Documentation=man:gsad(8) https://www.greenbone.net
+After=network.target gvmd.service
+Wants=gvmd.service
 
-# [Service]
-# Type=forking
-# User=gvm
-# Group=gvm
-# PIDFile=/run/gvm/gsad.pid
-# ExecStart=/usr/local/sbin/gsad --listen=${SERVER_IP} --port=9392
-# # ExecStart=/usr/local/sbin/gsad --listen=0.0.0.0 --port=9392
-# Restart=always
-# TimeoutStopSec=10
+[Service]
+Type=forking
+User=gvm
+Group=gvm
+PIDFile=/run/gvm/gsad.pid
+ExecStart=/usr/local/sbin/gsad --listen=${SERVER_IP} --port=9392
+# ExecStart=/usr/local/sbin/gsad --listen=0.0.0.0 --port=9392
+Restart=always
+TimeoutStopSec=10
 
-# [Install]
-# WantedBy=multi-user.target
-# Alias=greenbone-security-assistant.service
-# EOF
+[Install]
+WantedBy=multi-user.target
+Alias=greenbone-security-assistant.service
+EOF
 
-# sudo cp $BUILD_DIR/gsad.service /etc/systemd/system/
+sudo cp $BUILD_DIR/gsad.service /etc/systemd/system/
 
-# cat << EOF > $BUILD_DIR/ospd-openvas.service
-# [Unit]
-# Description=OSPd Wrapper for the OpenVAS Scanner (ospd-openvas)
-# Documentation=man:ospd-openvas(8) man:openvas(8)
-# After=network.target networking.service redis-server@openvas.service
-# Wants=redis-server@openvas.service
-# ConditionKernelCommandLine=!recovery
+cat << EOF > $BUILD_DIR/ospd-openvas.service
+[Unit]
+Description=OSPd Wrapper for the OpenVAS Scanner (ospd-openvas)
+Documentation=man:ospd-openvas(8) man:openvas(8)
+After=network.target networking.service redis-server@openvas.service
+Wants=redis-server@openvas.service
+ConditionKernelCommandLine=!recovery
 
-# [Service]
-# Type=forking
-# User=gvm
-# Group=gvm
-# RuntimeDirectory=ospd
-# RuntimeDirectoryMode=2775
-# PIDFile=/run/ospd/ospd-openvas.pid
-# ExecStart=/usr/local/bin/ospd-openvas --unix-socket /run/ospd/ospd-openvas.sock --pid-file /run/ospd/ospd-openvas.pid --log-file /var/log/gvm/ospd-openvas.log --lock-file-dir /var/lib/openvas --socket-mode 0o770
-# SuccessExitStatus=SIGKILL
-# Restart=always
-# RestartSec=60
+[Service]
+Type=forking
+User=gvm
+Group=gvm
+RuntimeDirectory=ospd
+RuntimeDirectoryMode=2775
+PIDFile=/run/ospd/ospd-openvas.pid
+ExecStart=/usr/local/bin/ospd-openvas --unix-socket /run/ospd/ospd-openvas.sock --pid-file /run/ospd/ospd-openvas.pid --log-file /var/log/gvm/ospd-openvas.log --lock-file-dir /var/lib/openvas --socket-mode 0o770
+SuccessExitStatus=SIGKILL
+Restart=always
+RestartSec=60
 
-# [Install]
-# WantedBy=multi-user.target
-# EOF
+[Install]
+WantedBy=multi-user.target
+EOF
 
-# sudo cp $BUILD_DIR/ospd-openvas.service /etc/systemd/system/
+sudo cp $BUILD_DIR/ospd-openvas.service /etc/systemd/system/
 
 # # Applying new units
 # # -------------------------------------------------------------------------------------------\
 
-# sudo systemctl daemon-reload
-
+sudo systemctl daemon-reload
 
 enable_sd ospd-openvas; enable_sd gvmd; enable_sd gsad
 
