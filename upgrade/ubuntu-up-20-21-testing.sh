f36cb242a2219fabec296b47c32538368c72d10d
/:) <g.yevgeniy.p@gmail.com>
2021-12-24 14:50:18 +0300

    + testing

--- upgrade/ubuntu-up-20-21-testing.sh
+++ upgrade/ubuntu-up-20-21-testing.sh
@@ -313,30 +313,30 @@ sudo -u gvm gvm-manage-certs -a
 # Gen systemd units
 # -------------------------------------------------------------------------------------------\
 
-# cat << EOF > $BUILD_DIR/gvmd.service
-# [Unit]
-# Description=Greenbone Vulnerability Manager daemon (gvmd)
-# After=network.target networking.service postgresql.service ospd-openvas.service
-# Wants=postgresql.service ospd-openvas.service
-# Documentation=man:gvmd(8)
-# ConditionKernelCommandLine=!recovery
+cat << EOF > $BUILD_DIR/gvmd.service
+[Unit]
+Description=Greenbone Vulnerability Manager daemon (gvmd)
+After=network.target networking.service postgresql.service ospd-openvas.service
+Wants=postgresql.service ospd-openvas.service
+Documentation=man:gvmd(8)
+ConditionKernelCommandLine=!recovery
 
-# [Service]
-# Type=forking
-# User=gvm
-# Group=gvm
-# PIDFile=/run/gvm/gvmd.pid
-# RuntimeDirectory=gvm
-# RuntimeDirectoryMode=2775
-# ExecStart=/usr/local/sbin/gvmd --osp-vt-update=/run/ospd/ospd-openvas.sock --listen-group=gvm
-# Restart=always
-# TimeoutStopSec=10
+[Service]
+Type=forking
+User=gvm
+Group=gvm
+PIDFile=/run/gvm/gvmd.pid
+RuntimeDirectory=gvm
+RuntimeDirectoryMode=2775
+ExecStart=/usr/local/sbin/gvmd --osp-vt-update=/run/ospd/ospd-openvas.sock --listen-group=gvm
+Restart=always
+TimeoutStopSec=10
 
-# [Install]
-# WantedBy=multi-user.target
-# EOF
+[Install]
+WantedBy=multi-user.target
+EOF
 
-# sudo cp $BUILD_DIR/gvmd.service /etc/systemd/system/
+sudo cp $BUILD_DIR/gvmd.service /etc/systemd/system/
 
 cat << EOF > $BUILD_DIR/gsad.service
 [Unit]
